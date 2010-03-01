1787418924ea2b876a4e0599fb618a7fc753a815
Jozef Kutej <jozef@kutej.net>
2010-03-01 20:35:49 +0100

    initial

---
+++ lib/CPAN/Patches/Plugin/Debian/SPc.pm
@@ -0,0 +1,33 @@
+package CPAN::Patches::Plugin::Debian::SPc;
+
+=head1 NAME
+
+CPAN::Patches::Plugin::Debian::SPc - Debian specific folders
+
+=cut
+
+use warnings;
+use strict;
+
+our $VERSION = '0.01';
+
+use File::Spec;
+
+sub _path_types {qw(
+	sharedstatedir
+)};
+
+=head1 PATHS
+
+=head2 prefix
+
+=head2 sharedstatedir
+
+Used to store Debian patch set and svn trunk
+
+=cut
+
+sub prefix     { use Sys::Path; Sys::Path->find_distribution_root(__PACKAGE__); };
+sub sharedstatedir { File::Spec->catdir(__PACKAGE__->prefix, 'sharedstate') };
+
+1;
